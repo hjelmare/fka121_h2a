eb9fe85e11f48da36c6c94c3f2f32b5f6f102626
hjelmare <hjelmare@users.noreply.github.com>
2014-12-04 19:52:39 +0100

    haha

--- ba_main.c
+++ ba_main.c
@@ -6,7 +6,7 @@
 #include "ba_func.h"
 
 #define PI 3.14159265
-#define boltzmannsConstant 1.3806488*pow(10,-23)  //m^2 kg s^-2 K^-1 RIGHT UNITS???????
+#define BOLTZMANN 1.3806488*pow(10,-23)
 int main()
 {
 
