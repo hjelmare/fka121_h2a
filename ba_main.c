a979f3c83080d7b4e64ee38c8a882eba5d6218a2
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-12-04 19:57:56 +0100

    written the constants

--- ba_main.c
+++ ba_main.c
@@ -8,9 +8,22 @@
 #define PI 3.14159265
 #define boltzmannsConstant 1.3806488*pow(10,-23)  //m^2 kg s^-2 K^-1 RIGHT UNITS???????
 int main()
-{
+{ 
 
+  double E_CuCu = -436; // meV
+  double E_ZnZn = -113; // meV
+  double E_CuZn = -294; // meV
+  double dE = E_CuCu + E_ZnZn - 2*ECuZn;
+  double T_c = 2*dE/boltzmannsConstant;
+  printf("the phase transition temperature is %e \n", T_c);
 
+  // open files to save values in.
+  FILE *valuesFile;
+  valuesFile = fopen("values.data","w");
+
+  fprintf(valuesFile,"%e\tphase transition temperature\n");
+
+  
 
   return 0;
 }
