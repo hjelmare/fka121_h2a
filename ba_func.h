7e1da219df2b11fcbfff352ed7e23710225109d8
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-12-06 16:18:24 +0100

    fixat en function som beräknar energin

--- ba_func.h
+++ ba_func.h
@@ -3,7 +3,8 @@
 #ifndef _MD_functions_h
 #define _MD_functions_h
 
-extern void InitializeSystem(int , double [][8], double [][8]);
+extern void InitializeSystem(int, double [][8], double [][8]);
+extern double getEnergy(int, double [][8], double [][8]);
 //extern void GetMean(double *, int, double *);
 //extern double GetNewValue(double, double);
 
