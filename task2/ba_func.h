fd19b4c1350bf40b0b3be5104ea6e365aff62b97
karlssonalbinka <karlsson.albin.ka@gmail.com>
2014-12-11 13:33:50 +0100

    short range order parameter

--- task2/ba_func.h
+++ task2/ba_func.h
@@ -3,6 +3,7 @@
 
 
 void InitializeLattice(int n, int lattice[], int type);
+double GetShortRangeOrder(int n, int latticeA[], int latticeB[], int neighboursToA[][8]);
 double GetEnergy(int n, int latticeA[], int latticeB[], int neighboursToA[][8], int neighboursToB[][8]);
 void InitializeNeighbourMatrices(int n, int GA[][8], int GB[][8]);
 
