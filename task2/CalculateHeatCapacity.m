a7cc8adf6917882bf7fa06152cb42f2f43418af6
hjelmare <hjelmare@users.noreply.github.com>
2014-12-18 16:56:42 +0100

    får vi ta jullov nu?

--- task2/CalculateHeatCapacity.m
+++ task2/CalculateHeatCapacity.m
@@ -3,28 +3,16 @@ clc
 
 k_B = 8.61734e-5;
 
-% get some data, import from file...
-energy = rand(1,1000000);
-T = 1;
+T = 900;
+fid = fopen('/media/My Passport/compPhys/T900.bin','r');
+energy = fread(fid,[20e6,3],'double');
+fclose(fid);
 
-nPoints = length(energy);
-blockSize = 100;
-nBlocks = nPoints / blockSize;
+prefactor = 1/(k_B * T^2);
 
-for i = 1:(nBlocks - 1)
-    bStart = 1+100*i;
-    bEnd = 100+100*i;
+energyVariance = var(energy);
+heatCapacity = prefactor * energyVariance;
 
-    blockAverageEnergy(i) = sum(energy(bStart:bEnd)) / blockSize;
-end
-
-energyVariance = var(blockAverageEnergy);
-heatCapacity = 1/(k_B * T^2) * energyVariance;
-
-meanEnergy = mean(blockAverageEnergy);
-fourthMoment = mean( (blockAverageEnergy - meanEnergy).^4 );
-
-heatVariance = 1/(nBlocks) * (fourthMoment - (nBlocks - 3)/(nBlocks - 1)*energyVariance^2);
-
-disp('   C                   Var(C)')
+disp('   C')
 disp([heatCapacity, heatVariance])
+
